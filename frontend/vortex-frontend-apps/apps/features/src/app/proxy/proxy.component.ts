c8f6cff6d8ca6cc5a36df56378e919795a363f9c
Srikanth Kaushik <srikanth.kaushik@gmail.com>
2023-03-05 20:51:10 -0500

    Added Features App

---
+++ frontend/vortex-frontend-apps/apps/features/src/app/proxy/proxy.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'vortex-apps-proxy',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './proxy.component.html',
+  styleUrls: ['./proxy.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ProxyComponent {}
